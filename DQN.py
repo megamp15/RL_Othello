c4980aa72988d1b3bb2b073d680267ad0e4fce7c
Mahir Pirmohammed <pmahir15@gmail.com>
2024-02-17 19:49:09 -0600

    Generalized PyTorch NN using RL example from docs

--- DQN.py
+++ DQN.py
@@ -3,14 +3,14 @@ import torch.nn as nn
 
 # Define model
 class NeuralNet(nn.Module):
-    def __init__(self, state_dim, num_actions, epsilon):
+    def __init__(self, input_dim, output_dim):
         super().__init__()
-        self.state_dim = state_dim
-        self.num_actions = num_actions
-        self.epsilon = epsilon
-        print(f"state_dim: {state_dim}, num_actions: {num_actions}, epsilon: {epsilon}")
+        self.input_dim = input_dim
+        self.channel, self.height, self.width = self.input_dim
+        self.output_dim = output_dim
+        print(f"input_dim: {input_dim}, output_dim: {output_dim}")
         self.device = self.device()
-        self.network = self.network()
+        self.network = self.construct_network()
 
     def device(self):
         """ Retrive device for tensorflow"""
@@ -18,13 +18,21 @@ class NeuralNet(nn.Module):
         print(f"Using {device} device")
         return device
     
-    def network(self):
+    def construct_network(self):
+        """
+        JUST TO TEST - COPY OF THE MARIO MINI CNN STRUCTURE FROM https://pytorch.org/tutorials/intermediate/mario_rl_tutorial.html
+        """
         return nn.Sequential(
-            # nn.Conv2d(in_channels=0, out_channels=32, kernel_size=8, stride=4),
-            # nn.ReLU(),
-            # nn.linear(),
-            # nn.ReLU(),
-            # nn.linear(128,num_actions),
+            nn.Conv2d(in_channels=self.channel, out_channels=32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(3136, 512),
+            nn.ReLU(),
+            nn.Linear(512, self.output_dim),
         )
 
     def forward(self, state):
@@ -32,14 +40,7 @@ class NeuralNet(nn.Module):
         assert Q.requires_grad, "Q-Values must be a Torch Tensor with a Gradient"
         return Q
     
-    def loss():
-        pass
-
-    def train():
-        pass
-
-    def action():
-        pass
+    
 
 
 class DQN(NeuralNet):
@@ -49,3 +50,12 @@ class DQN(NeuralNet):
     def model(self):
         model = NeuralNet().to(self.device)
         print(model)
+
+    def loss():
+        pass
+
+    def train():
+        pass
+
+    def action():
+        pass
