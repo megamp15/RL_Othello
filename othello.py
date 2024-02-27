2c4eaee15e853f2829acb5ce3b992da803dc2ed9
JoshClark-Drexel <joshclark431335@gmail.com>
2024-02-27 12:45:30 -0500

    changed some comments, and some minor code related to type hints and enums. Should work the same

--- othello.py
+++ othello.py
@@ -6,70 +6,82 @@ import numpy as np
 import matplotlib.pyplot as plt
 import Agents
 
+class OBS_SPACE(Enum):
+    """
+    The observation spaces available through Gymnasium
+    """
+    RGB = "rgb"
+    GRAY = "grayscale"
+    RAM = "ram"
+
+
+class RENDER_MODE(Enum):
+    HUMAN = "human"
+    RGB = "rgb_array"
+
 
 class Othello():
     """
-    The Gymnasium Atari Game Othello Environment
+    The environment for Othello that wraps the Gymnasium Atari Environment
     """
-    def __init__(self, render_mode, observation_type, record_video):
-        self.RENDER_MODE = render_mode
-        self.OBS_TYPE = observation_type
-        self.RECORD_VIDEO = record_video 
-        self.ENV = self.setup_env()
+    def __init__(self, render_mode:RENDER_MODE, observation_type:OBS_SPACE, record_video:bool):
+        self.render_mode = render_mode
+        self.obs_type = observation_type
+        self.record_video = record_video 
+        self.env = self.setup_env()
 
-
-    def setup_env(self):
+    def setup_env(self) -> gym.Env:
         """
         Construct and return the Gymnasium Atari Environment for Othello
         """
-        if self.RECORD_VIDEO:
-            self.RENDER_MODE = render_mode.RGB.value
-        env = gym.make("ALE/Othello-v5", render_mode=self.RENDER_MODE, obs_type=self.OBS_TYPE)
+        if self.record_video:
+            self.render_mode = RENDER_MODE.RGB
+        env = gym.make("ALE/Othello-v5", render_mode=self.render_mode.value, obs_type=self.obs_type.value)
         # Resize obs_space from 210 x 160 -> 105 x 80 to conserve data space but same resolution
         env = ResizeObservation(env, (105, 80))
-        if self.RECORD_VIDEO:
-            env = RecordVideo(env=env, video_folder="./recordings", name_prefix="test-video", episode_trigger=lambda x: x % 2 == 0)
+        if self.record_video:
+            env = RecordVideo(env, video_folder="./recordings", name_prefix="test-video",\
+                              episode_trigger=lambda x: x % 2 == 0)
         return env
 
-
-    def run(self):
+    def run(self) -> None:
         """
         Run Othello Environment for Testing
         """
-        observation, info = self.ENV.reset()
+        observation, _ = self.env.reset()
         obs = self.preprocess_obs(observation)
-        if self.RECORD_VIDEO:
-            self.ENV.start_video_recorder()
+        if self.record_video:
+            self.env.start_video_recorder()
 
         for _ in range(1000):
-            action = self.ENV.action_space.sample() 
-            observation, reward, terminated, truncated, info = self.ENV.step(action)
+            action = self.env.action_space.sample() 
+            observation, reward, terminated, truncated, _ = self.env.step(action)
             # obs = self.preprocess_obs(observation)
-            self.ENV.render()
+            self.env.render()
 
             if terminated or truncated:
-                observation, info = self.ENV.reset()
+                observation, _ = self.env.reset()
 
-        if self.RECORD_VIDEO:
-            self.ENV.close_video_recorder()
+        if self.record_video:
+            self.env.close_video_recorder()
 
-        self.ENV.close()
+        self.env.close()
 
-    def run_DQN(self, episodes=1, T=10000):
+    def run_DQN(self, episodes=1, max_turns=10000) -> None:
         # Get initial observation to preprocess and setup Agent
-        observation, info = self.ENV.reset()
+        observation, _ = self.env.reset()
         obs = self.preprocess_obs(observation)
-        DQN_Agent = Agents.DQN(env=self.ENV, state_shape=obs.shape, num_actions=self.ENV.action_space.n, epsilon=0.5)
+        DQN_Agent = Agents.DQN(self.env, obs.shape, num_actions=self.env.action_space.n, epsilon=0.5)
 
         rewards = []
-        for e in range(episodes):
-            observation, info = self.ENV.reset()
+        for _ in range(episodes):
+            observation, _ = self.env.reset()
             state = self.preprocess_obs(observation)
             r = 0
-            for t in range(T):
+            for _ in range(max_turns):
                 # print(f"t: {t}")
                 action = DQN_Agent.action(state)
-                observation, reward, terminated, truncated, info = self.ENV.step(action)
+                observation, reward, terminated, truncated, _ = self.env.step(action)
                 next_state = self.preprocess_obs(observation)
                 state = next_state
                 r += reward
@@ -79,40 +91,25 @@ class Othello():
         print(f"Len rewards: {len(rewards)}")
         print(f"Rewards: {np.mean(rewards)}")
 
-
-
-    def preprocess_obs(self, obs):
+    def preprocess_obs(self, obs:np.ndarray) -> np.ndarray:
         """
-        Crop the observation image to only look at the board. 
-        Switch Channel from last index to first index for PyTorch: [c, h, w]
+        Crop the observation image to only look at the board.
+        This should come in as [105, 80, 3] for RGB or [105, 80] for GRAY
+        Rotate color channel to first (or add if GRAY), and add another in first for batch size
+        Output size should be [batch_size, n_channel, height, width] = [1, 1 (or 3), 90, 66]
         Uncomment the imshow to see the images before the axes are changed
         """
         # OBS_TYPE = RGB - Don't think we need this. We should train on GRAY scale images anyways
-        if len(obs.shape) > 2: 
+        if obs.ndim > 2:
+            # Crop
             obs_cropped = obs[8:-7, 6:-8, :]
-            # plt.imshow(obs_cropped)
-            # plt.show()
+            # Move color channel in front
             obs_cropped = np.moveaxis(obs_cropped, -1, 0)
         # OBS_TYPE = GRAY
-        else: 
+        else:
+            # Crop 
             obs_cropped = obs[8:-7, 6:-8]
-            # plt.imshow(obs_cropped)
-            # plt.show()
-            obs_cropped = [obs_cropped]
-        obs = np.array(obs_cropped)
-        obs = np.expand_dims(obs, axis=0)
-        # SHAPE is as follows [batch size=1 x channel=(1 if GRAY and 3 if RGB) x Height=90 x Width=66] 
-        # print(obs.shape) 
-        return obs
-
-class obs_space(Enum):
-    """
-    The observation spaces available through Gymnasium
-    """
-    RGB = "rgb"
-    GRAY = "grayscale"
-    RAM = "ram"
-
-class render_mode(Enum):
-    HUMAN = "human"
-    RGB = "rgb_array"
+            # Add a dimension in front for color channel
+            obs_cropped = np.expand_dims(obs_cropped, axis=0)
+        # Add another dimension in front for batch size
+        return np.expand_dims(obs_cropped, axis=0)
