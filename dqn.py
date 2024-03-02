3ed62d4730088eedbc36690149f21543aa316bc5
Mahir Pirmohammed <pmahir15@gmail.com>
2024-03-02 15:34:34 -0600

    Minor bug fixes. Does not run fully in RGB mode

--- dqn.py
+++ dqn.py
@@ -91,7 +91,7 @@ class DQN(DeepAgent):
 class DDQN(DeepAgent):
     
     def __init__(self, env:gym.Env, state_shape:np.ndarray, num_actions:int, epsilon:float, alpha:float, gamma:float, sync_interval:int,loss_func = nn.SmoothL1Loss):
-        super().__init__(self, env, state_shape, num_actions, epsilon, alpha, gamma, sync_interval,loss_func)
+        super().__init__(env, state_shape, num_actions, epsilon, alpha, gamma, sync_interval, loss_func)
         self.target_net = NeuralNet(state_shape, num_actions)
         # Copy inital weights from Q Network into the target network
         self.target_net.load_state_dict(self.network.state_dict())
@@ -132,7 +132,7 @@ class DDQN(DeepAgent):
 class DuelDQN(DeepAgent):
     
     def __init__(self, env:gym.Env, state_shape:np.ndarray, num_actions:int, epsilon:float, alpha:float, gamma:float, sync_interval:int,loss_func = nn.SmoothL1Loss):
-        super().__init__(self, env, state_shape, num_actions, epsilon, alpha, gamma, sync_interval,loss_func)
+        super().__init__(env, state_shape, num_actions, epsilon, alpha, gamma, sync_interval,loss_func)
         
         self.value_net = NeuralNet(state_shape, 1)
         self.advantage_net = self.network
