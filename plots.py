8aa89d50d54f6f2ff8fa0d82f28545dffb0c60de
JoshClark-Drexel <joshclark431335@gmail.com>
2024-03-13 18:54:12 -0400

    converted log file processing from pandas to numpy to save an import

--- plots.py
+++ plots.py
@@ -1,14 +1,17 @@
 import os
-import pandas as pd
+# import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 class LogFileAnalyzer:
     def __init__(self, log_dir):
         self.log_dir = log_dir
-        self.ep_lengths = []
-        self.ep_avg_losses = []
-        self.ep_avg_qs = []
-        self.ep_rewards = []
+        self.metrics : dict[str,dict[str,list[float]]] = {
+            'Mean_Reward' : {},
+            'Mean_Length' : {},
+            'Mean_Loss' : {},
+            'Mean_Q-Value' : {}
+        }
 
     def process_log_files(self):
         log_files = os.listdir(self.log_dir)
@@ -19,22 +22,23 @@ class LogFileAnalyzer:
                 self.process_log_file(file_path, label)
 
     def process_log_file(self, file_path, label):
-        df = pd.read_csv(file_path)
-        self.ep_lengths.append((df['#Episode'], label))
-        self.ep_avg_losses.append((df['MeanLoss'], label))
-        self.ep_avg_qs.append((df['MeanQValue'], label))
-        self.ep_rewards.append((df['MeanReward'], label))
+        _, _, _, ep_rewards, ep_lengths, ep_avg_losses, ep_avg_qs, _, _ \
+            = np.loadtxt(file_path, converters={8 : lambda x: None}, delimiter=',',unpack=True)
+        self.metrics['Mean_Reward'][label] = ep_rewards
+        self.metrics['Mean_Length'][label] = ep_lengths
+        self.metrics['Mean_Loss'][label] = ep_avg_losses
+        self.metrics['Mean_Q-Value'][label] = ep_avg_qs
 
     def generate_plots(self):
-        for metric in ["ep_lengths", "ep_avg_losses", "ep_avg_qs", "ep_rewards"]:
+        for metric_name, metric_data in self.metrics.items():
             plt.clf()
-            for data, label in getattr(self, metric):
-                plt.plot(data, label=label)
+            for model_name in metric_data.keys():
+                plt.plot(metric_data[model_name], label=model_name)
             plt.xlabel('Episode')
-            plt.ylabel(metric.split('_')[-1].capitalize())
-            plt.title(f'{metric.capitalize()} over Episodes')
+            plt.ylabel(metric_name.split('_')[1].capitalize())
+            plt.title(f'{metric_name.capitalize()} over Episodes')
             plt.legend()
-            plt.savefig(f"{self.log_dir}/{metric}_plot.png")
+            plt.savefig(f"{self.log_dir}/{metric_name}_plot.png")
 
 if __name__ == "__main__":
     log_dir = "./logs_to_plot"
